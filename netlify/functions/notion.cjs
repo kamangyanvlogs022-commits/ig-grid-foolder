0a74b3af83de632e7addcdd14e1666638908abf2
socialsbyjuvi <kamangyanvlogs022@gmail.com>
2026-02-09 19:15:58 +0800

    Update notion.cjs

--- netlify/functions/notion.cjs
+++ netlify/functions/notion.cjs
@@ -9,27 +9,20 @@ exports.handler = async () => {
     const response = await notion.databases.query({
       database_id: process.env.NOTION_DATABASE_ID,
       sorts: [
-        {
-          property: "Publish Date",
-          direction: "ascending",
-        },
+        { property: "Publish Date", direction: "ascending" }
       ],
     });
 
     const data = response.results.map(page => {
       const files = page.properties["Files & media"]?.files || [];
 
-      let imageUrl = "";
-      if (files.length > 0) {
-        imageUrl =
-          files[0].type === "external"
-            ? files[0].external.url
-            : files[0].file.url;
-      }
-
       return {
         title: page.properties.Name?.title?.[0]?.plain_text || "",
-        url: imageUrl,
+        url: files.length > 0
+          ? (files[0].type === "external"
+              ? files[0].external.url
+              : files[0].file.url)
+          : "",
         date: page.properties["Publish Date"]?.date?.start || "",
       };
     });
@@ -38,9 +31,11 @@ exports.handler = async () => {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     };
+
   } catch (error) {
     return {
       statusCode: 500,
